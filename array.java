a474d51280c18f31054614e72357483dfa7a42d5
Safkat khan Tafsir <safkatkhan420@gmail.com>
2022-07-27 23:59:02 +0600

    first commit

---
+++ array.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+public class array{
+    public static void main( String[] args){
+      //  Scanner input =new sca
+        System.out.println("Enter 5 Integer Number :" );
+
+
+
+    }
+
+}
---
+++ input.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+public class input {
+    public static void main(String[] args){
+        Scanner name = new Scanner(System.in);
+        int inputnum;
+        System.out.println("Input Your Number : ");
+    
+        inputnum=name.nextInt();
+        
+        System.out.println("Your Inputed Number : "+inputnum);
+
+
+    }
+    
+}
