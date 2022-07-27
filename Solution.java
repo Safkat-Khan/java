57e740d4523929b451c7a2b257d270acef92ef38
Safkat khan Tafsir <safkatkhan420@gmail.com>
2022-07-28 03:26:46 +0600

    problem solving

---
+++ Solution.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		int num1;
+		Scanner number1=new Scanner(System.in);
+		num1=number1.nextInt();
+
+		int num2;
+		Scanner number2=new Scanner(System.in);
+		num2=number2.nextInt();
+
+		int sum=num1+num2;
+		System.out.println(+sum);
+	}
+}
---
+++ copycat.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+public class copycat {
+
+	public static void main(String[] args) {
+		System.out.println("Input Your Number :");
+		int Store;
+		Scanner number=new Scanner(System.in);
+		Store=number.nextInt();
+	System.out.println("Output :"+Store);
+}
+}
