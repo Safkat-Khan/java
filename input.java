1041a793af25c2dfcae4b7329b7b173aa5bdd683
Safkat khan Tafsir <safkatkhan420@gmail.com>
2022-07-28 00:19:39 +0600

    String Inputed

--- input.java
+++ input.java
@@ -2,6 +2,15 @@ import java.lang.*;
 import java.util.*;
 public class input {
     public static void main(String[] args){
+
+System.out.println("Enter Your Name Here");
+String Store;
+
+Scanner name=new Scanner(System.in);
+Store=name.nextLine();
+
+System.out.println("Your Entered Number IS :" +Store);
+
 System.out.println("Please Inter Any Integer Number :");
 int store;
 Scanner number=new Scanner(System.in);
@@ -11,13 +20,7 @@ store=number.nextInt();
 System.out.println("You Are Interd Number is :" +store);
 
 
-Scanner name = new Scanner(System.in);
-int inputnum;
-System.out.println("Input Your Number : ");
 
-inputnum=name.nextInt();
-
-System.out.println("Your Inputed Number : "+inputnum);
 
     }
     
