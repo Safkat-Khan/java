c4c15304b1ac408da03b1df33c7f1b48820b2e85
Safkat khan Tafsir <safkatkhan420@gmail.com>
2022-07-28 00:09:14 +0600

    second Input Added

--- input.java
+++ input.java
@@ -2,14 +2,22 @@ import java.lang.*;
 import java.util.*;
 public class input {
     public static void main(String[] args){
-        Scanner name = new Scanner(System.in);
-        int inputnum;
-        System.out.println("Input Your Number : ");
-    
-        inputnum=name.nextInt();
-        
-        System.out.println("Your Inputed Number : "+inputnum);
+System.out.println("Please Inter Any Integer Number :");
+int store;
+Scanner number=new Scanner(System.in);
 
+store=number.nextInt();
+
+System.out.println("You Are Interd Number is :" +store);
+
+
+Scanner name = new Scanner(System.in);
+int inputnum;
+System.out.println("Input Your Number : ");
+
+inputnum=name.nextInt();
+
+System.out.println("Your Inputed Number : "+inputnum);
 
     }
     
